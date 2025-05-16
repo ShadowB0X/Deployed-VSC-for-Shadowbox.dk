6ae757b43af02c2e043cc65a7b0721228a9a57ed
Z3NA <zanaboss@live.dk>
2025-05-16 22:50:32 +0200

    dddddd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
 import Timer from './components/Timer'
 
 function App() {
   return (
     <div>
-      <Timer />
+      <BrowserRouter>
+      <App />
+    </BrowserRouter>
     </div>
   )
 }
