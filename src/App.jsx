a28bacd0a86ba0805628187d2cba2a982828ef20
Z3NA <zanaboss@live.dk>
2025-05-16 23:06:40 +0200

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter } from 'react-router-dom'
-import Timer from './components/Timer'
+import { Routes, Route } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import VisionPage from './pages/VisionPage'
+import EndpointPage from './pages/EndPointPage'
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/vision" element={<VisionPage />} />
+      <Route path="/endpoints" element={<EndpointPage />} />
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+
 // Import your global CSS file here
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
